1f6f449a653c05ce6e1ae203fd7ef98ff8bfc562
radovansurlak <radik2812@gmail.com>
2017-08-04 22:08:59 +0200

    changed API location and radius + made small styling changes

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ var app = new Vue({
   }
 });
 
-fetch('https://api.foursquare.com/v2/venues/search?&categoryId=4d4b7105d754a06376d81259&radius=300&client_id=2TC0GHMY3FTKAY5Y3WSXWTYUT3EY0CL11DOTPSD5BELKYDVU&client_secret=ISIT3PFVCZORCQA0J3YGFOJOQ1C3GUZPUDFUKL3LKGRR4O5N&v=20170117&ll=48.168088,17.138037')
+fetch('https://api.foursquare.com/v2/venues/search?&categoryId=4d4b7105d754a06376d81259&radius=3000&client_id=2TC0GHMY3FTKAY5Y3WSXWTYUT3EY0CL11DOTPSD5BELKYDVU&client_secret=ISIT3PFVCZORCQA0J3YGFOJOQ1C3GUZPUDFUKL3LKGRR4O5N&v=20170117&ll=56.108199809,10.162181854')
 .then(function(response) {
   response.json().then(function(data){
     data.response.venues.map((venue) => {
+      if (venue.location.address === undefined){
+        venue.location.address = venue.location.formattedAddress[0]
+      }
       app._data.pubs.push({
         name: venue.name,
         category: venue.categories[0].shortName,
@@ -23,3 +26,5 @@ fetch('https://api.foursquare.com/v2/venues/search?&categoryId=4d4b7105d754a0637
 }).catch(function(err) {
 	console.log('Error:' + err);
 });
+
+// address: venue.location.address,
